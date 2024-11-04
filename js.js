781ad41a86299f1f7beaca1944a942df6b7e6055
shihas07 <shihas732@gmail.com>
2024-11-04 23:42:10 +0530

    bruteforce

--- js.js
+++ js.js
@@ -597,3 +597,33 @@
 //   }
 // };
 // console.log(twoSum([2, 7, 11, 15], 9));
+
+//kadens algorithem
+
+// Task: Kadane's Algorithm
+
+// Input: [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+// Output: 6 (Subarray [4, -1, 2, 1] has the maximum sum)
+
+const kadensSum = (arr) => {
+  let subarr = [];
+
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      subarr.push(arr.slice(i, j));
+    }
+  }
+  console.log(subarr.length);
+
+    const result= subarr.map(item=>item.reduce((a,b)=>a+b,0))
+    const max= result.sort((a,b)=>b-a)
+    // subarr.map(item => item.reduce((a, b) => a + b, 0));
+    // console.log(max)
+
+      return max[0]
+  // return subarr;
+};
+
+console.log(kadensSum([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
